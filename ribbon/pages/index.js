6958af388d0224dce715e3331f16110ac98506e1
Mitch Anderson <67846917+MitchA29@users.noreply.github.com>
2022-02-23 13:04:50 -0600

    Created the header bar of application.

---
+++ ribbon/pages/index.js
@@ -0,0 +1,17 @@
+
+import Head from 'next/head'
+import Header from "../components/Header";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Ribbon</title>
+      </Head>
+
+      <Header />
+    </div>
+  )
+}
+
